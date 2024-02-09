df7d8cec4e64bc63f59403c220725d9543f81e8c
Francois Roewer-Despres <francois-rd@users.noreply.github.com>
2024-02-09 18:21:22 -0500

    Add TermUnFormatter.

--- src/accord/components/formatter.py
+++ src/accord/components/formatter.py
@@ -4,3 +4,8 @@ from ..base import Term
 class TermFormatter:
     def format(self, term: Term, language: str, *args, **kwargs) -> Term:
         raise NotImplementedError
+
+
+class TermUnFormatter:
+    def unformat(self, term: Term, *args, **kwargs) -> Term:
+        raise NotImplementedError
