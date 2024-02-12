c55ebcb154c612594c8ebec1b380db5da4a370f6
Francois Roewer-Despres <francois-rd@users.noreply.github.com>
2024-02-12 13:35:40 -0500

    Fixed import.

--- src/accord/base/__init__.py
+++ src/accord/base/__init__.py
@@ -1,5 +1,5 @@
 from .case import Case, GenericCaseLink, RelationalCaseLink
-from .qa import InstantiatedQAGroup, Label, QAData, QAGroup, QAGroupId, QAPrompt
+from .qa import Label, QAData, QAGroup, QAGroupId, QAPrompt
 from .relation import Relation, RelationId, RelationType
 from .template import GenericTemplate, RelationalTemplate, Template
 from .variable import GenericVariable, Term, Variable, VarId
