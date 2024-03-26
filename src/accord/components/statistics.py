dff5e7fd96dc49e13f7e2722b0d97af3a5362d81
Francois Roewer-Despres <francois-rd@users.noreply.github.com>
2024-03-25 20:05:06 -0400

    Add max counts to tree statistics.

--- src/accord/components/statistics.py
+++ src/accord/components/statistics.py
@@ -1,14 +1,21 @@
 from accord.base import RelationalTree
 
 
+def max_hops(tree: RelationalTree) -> int:
+    return len(tree.templates)
+
+
 def n_hop_factory(tree: RelationalTree, title: str = "Reasoning hops") -> dict:
     stats = {"title": title}
-    stats.update({i: 0 for i in range(len(tree.templates))})
+    stats.update({i: 0 for i in range(max_hops(tree) + 1)})
     return stats
 
 
+def max_af_vars(tree: RelationalTree) -> int:
+    return len(tree.unique_variable_ids()) - 2
+
+
 def af_vars_factory(tree: RelationalTree, title: str = "Number AF variables") -> dict:
-    max_af_vars = len(tree.unique_variable_ids()) - 2
     stats = {"title": title}
-    stats.update({i: n_hop_factory(tree) for i in range(max_af_vars + 1)})
+    stats.update({i: n_hop_factory(tree) for i in range(max_af_vars(tree) + 1)})
     return stats
